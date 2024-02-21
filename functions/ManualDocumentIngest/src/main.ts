f2603909d3ccecfedc20be476a2a92dea8e5b84a
Leo Keil <leolele19@gmail.com>
2024-02-21 21:22:53 +0100

    Better Debug

--- functions/ManualDocumentIngest/src/main.ts
+++ functions/ManualDocumentIngest/src/main.ts
@@ -40,28 +40,13 @@ export default async ({ req, res, log, error }: any) => {
   if (Bun.env["DEBUG_LOG"])
   {
     log("Debug Log enabled!")
-    log("Request Body:")
-    //log(req.body)
   }
 
-  // Parse jsonImport
-  // Get newest import file
-  /*const importDocuments = await databases.listDocuments(Bun.env["DATABASE_IMPORTS_ID"] || '', Bun.env["COLLECTION_IMPORTS_JSON_ID"] || '', [Query.orderDesc("$createdAt"), Query.limit(1)])
-  const importDocumentId = importDocuments.documents.at(0)?.$id
-  if (Bun.env["DEBUG_LOG"])
+  //Split payload and add individual entries to db
+  for (var entry of req.body)
   {
-    log("Found import document with ID:")
-    log(importDocumentId)
+    log(entry.name)
   }
-  // Get file content
-  const importDocument = await databases.getDocument(Bun.env["DATABASE_IMPORTS_ID"] || '', Bun.env["COLLECTION_IMPORTS_JSON_ID"] || '', importDocumentId || '') as importDocument
-  const importText = importDocument.content
-  const importJson = JSON.parse(importText)
-  if (Bun.env["DEBUG_LOG"])
-  {
-    log("File content:")
-    log(importJson)
-  }*/
   
 
   // Return
