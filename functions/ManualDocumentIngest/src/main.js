4ad6434565222a1ae33c5340432a845dc81faf36
Leo Keil <leolele19@gmail.com>
2024-02-21 22:36:55 +0100

    Typofix

--- functions/ManualDocumentIngest/src/main.js
+++ functions/ManualDocumentIngest/src/main.js
@@ -26,7 +26,7 @@ export default async ({ req, res, log, error }) => {
   }
 
   //Split payload and add individual entries to db
-  var payload = JSON.parse(Jreq.body)
+  var payload = JSON.parse(req.body)
   payload.forEach((importItem) => {
     log(importItem.name)
   })
