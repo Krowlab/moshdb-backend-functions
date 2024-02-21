4e05f26d0671a48e5e4693222816d7d454f0e97b
Leo Keil <leolele19@gmail.com>
2024-02-21 21:28:13 +0100

    Meow

--- functions/ManualDocumentIngest/src/main.ts
+++ functions/ManualDocumentIngest/src/main.ts
@@ -45,7 +45,7 @@ export default async ({ req, res, log, error }: any) => {
   //Split payload and add individual entries to db
   for (var entry of req.body)
   {
-    log(entry.name)
+    log(typeof req.body)
   }
   
 
